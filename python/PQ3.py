fc4b8456e418a5ca5e48a69c48aa173801de6d53
SHETYGURU <shettygururaj279@gmail.com>
2026-07-17 11:54:24 +0530

    Updated Code

---
+++ python/PQ3.py
@@ -0,0 +1,9 @@
+def max_freq_char(strng):
+    freq={}
+
+    for char in strng:
+        freq[char] = freq.get(char,0)+1
+    max_freq=max(freq,key=freq.get)
+    return max_freq,freq[max_freq]
+
+print(max_freq_char("uGururaj"))
